470bd123aaff309c93edf8e4cf6e66ea0dc72c74
reese <rjkaz@comcast.net>
2021-09-07 16:12:44 -0400

    starting module 47

--- models/User.js
+++ models/User.js
@@ -4,24 +4,24 @@ const mongoose = require('mongoose');
 // create a user schema that takes in an object of properties
 
 const UserSchema = mongoose.Schema({
-    name: {
-        type: String, 
-        required: true
-    },
-    email: {
-        type: String, 
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String, 
-        required: true
-    },
-    date: {
-        type: Date, 
-        default: Date.now
-    },
-}); 
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-//export and pass into the export the user schema we just created 
-mongoose.model('user', UserSchema);
+//export and pass into the export the user schema we just created
+module.exports = mongoose.model('user', UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,78 @@
-// we will bring in express so we can use the route
-
 const express = require('express');
-// create a variable called router and set that to express.Router (with capital R)
 const router = express.Router();
-// now when we do our routes, we don't do app.post, we do route.post
-
-// Bringing in the express validator check
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
-// bring in the User.js schema
-const User = require('../models/User');
+const User = require('../models/User.js');
 
+router.post(
+  '/',
+  [
+    check('name', 'Please add name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+module.exports = router;
+
+// we will bring in express so we can use the route
+// create a variable called router and set that to express.Router (with capital R)
+// Bringing in the express validator check
+// bring in the User.js schema
 // just to clarify, when you are making a get request, you are getting data. A post request is the opposite, you are giving data
 // and put requests are to update data that is already there, and a delete request is obviously to delete.
 // these are the 4 main methods in which we will be using
@@ -23,21 +85,5 @@ const User = require('../models/User');
 // now for this we will just put '/' because we already defined that route to say /api/users in the server.js file
 // so this router.post will also take in a req and a res, and a arrow function to a res.send
 // and in that res.send, we'll send in a message to register a user
-router.post('/', [
-  check('name', 'Please add name')
-    .not()
-    .isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
-], 
-(req, res) => {
-  const errors = validationResults(req);
-  if(!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  res.send('passed');
-});
 
 // now we have to export the router, otherwise it won't work
-
-module.exports = router;
