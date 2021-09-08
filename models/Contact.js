b8530f7db5814384804f64224ef39dc64572600d
reese <rjkaz@comcast.net>
2021-09-07 23:30:13 -0400

    section 7 finished

---
+++ models/Contact.js
@@ -0,0 +1,33 @@
+// bring in Mongoose
+const mongoose = require('mongoose');
+
+// create a user schema that takes in an object of properties
+
+const ContactSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: 'personal',
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//export and pass into the export the user schema we just created
+module.exports = mongoose.model('contact', ContactSchema);
