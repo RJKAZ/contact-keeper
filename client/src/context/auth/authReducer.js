99197ff89cf87c4ee964f7df79fca78764ec9710
RJ <rjkaz@comcast.net>
2021-09-10 23:38:05 -0400

    register and login forms

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import ContactContext from './contactContext';
-import contactReducer from './contactReducer';
+import ContactContext from '../contact/contactContext';
+import contactReducer from '../contact/contactReducer';
 import {
   ADD_CONTACT,
   DELETE_CONTACT,
