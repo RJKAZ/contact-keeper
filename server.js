09b0f4c841fd071d1d5c6f47b0c0a17c58ca24b9
reese <rjkaz@comcast.net>
2021-09-16 14:02:45 -0400

    final commit before deploying

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // we will use common js syntax to bring in express (react itself uses import)
 const connectDB = require('./config/db'); // bringing in the connectDB
+const path = require('path');
 
 const app = express(); // we will initiailize express into a variable called app
 
@@ -8,7 +9,6 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-
 
 // here we will add an endpoint route for testing purposes using a get request to the homepage with ('/')
 // and then it takes in a arrow function with a request and a response which will send a hello world
@@ -21,6 +21,17 @@ app.get('/', (req, res) =>
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000; // variable for the port, we are setting it like this so it will look for an env variable first, and it not, it will use port 5000
 
